ca62ed0eda7b733529c1d0b5a0a72739f42f92fd
Weijie Wang <duangsir518@163.com>
2024-12-13 17:01:27 +0100

    New file
    # adjust centaruo_env.py
    according to  anymal to modify centauro_env.py

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/centauro/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/centauro/__init__.py
@@ -10,20 +10,31 @@ Humanoid locomotion environment.
 import gymnasium as gym
 
 from . import agents
-from .centauro_env import CentauroEnv, CentauroEnvCfg
 
 ##
 # Register Gym environments.
 ##
 
 gym.register(
-    id="Isaac-Centauro-Direct-v0",
-    entry_point="omni.isaac.lab_tasks.direct.centauro:CentauroEnv",
+    id="Isaac-Velocity-Flat-Centauro-Direct-v0",
+    entry_point=f"{__name__}.centauro_env:CentauroEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": CentauroEnvCfg,
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "env_cfg_entry_point": f"{__name__}.centauro_env_cfg:CentauroFlatEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_flat_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCFlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Velocity-Rough-Centauro-Direct-v0",
+    entry_point=f"{__name__}.centauro_env:CentauroEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.anymal_c_env_cfg:CentauroRoughEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_rough_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCRoughPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
 )
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/centauro/agents/rsl_rl_ppo_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/centauro/agents/rsl_rl_ppo_cfg.py
@@ -13,29 +13,58 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
 
 
 @configclass
-class HumanoidPPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 32
-    max_iterations = 1000
+class AnymalCFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 500
     save_interval = 50
-    experiment_name = "humanoid_direct"
-    empirical_normalization = True
+    experiment_name = "anymal_c_flat_direct"
+    empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
-        actor_hidden_dims=[400, 200, 100],
-        critic_hidden_dims=[400, 200, 100],
+        actor_hidden_dims=[128, 128, 128],
+        critic_hidden_dims=[128, 128, 128],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.0,
+        entropy_coef=0.005,
         num_learning_epochs=5,
         num_mini_batches=4,
-        learning_rate=1.0e-4,
+        learning_rate=1.0e-3,
         schedule="adaptive",
         gamma=0.99,
         lam=0.95,
-        desired_kl=0.008,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
+
+
+@configclass
+class AnymalCRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 1500
+    save_interval = 50
+    experiment_name = "anymal_c_rough_direct"
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
         max_grad_norm=1.0,
     )
