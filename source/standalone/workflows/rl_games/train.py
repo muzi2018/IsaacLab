76cab7bd5360555b2eed95eb4e65339dffaacada
Weijie Wang <duangsir518@163.com>
2024-12-13 16:06:03 +0100

    syn
    just syn

--- source/standalone/workflows/rl_games/train.py
+++ source/standalone/workflows/rl_games/train.py
@@ -12,11 +12,7 @@ import sys
 
 from omni.isaac.lab.app import AppLauncher
 
-"""
-=============================================================================================
-add argparse arguments
-=============================================================================================
-"""
+
 parser = argparse.ArgumentParser(description="Train an RL agent with RL-Games.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
 parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
