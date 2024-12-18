bca975fcb75e1e368288fd12373be4ba03c57e74
Weijie Wang <duangsir518@163.com>
2024-12-18 10:25:01 +0100

    New configuration
    New centauro configuration

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/centauro.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/centauro.py
@@ -19,18 +19,19 @@ from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
 CENTAURO_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         usd_path="/home/wang/IsaacLab/source/extensions/omni.isaac.lab_assets/data/Robots/Centauro/centauro.usd",
-        activate_contact_sensors=False,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            retain_accelerations=False,
-            linear_damping=0.0,
-            angular_damping=0.0,
+            rigid_body_enabled=True,
             max_linear_velocity=1000.0,
             max_angular_velocity=1000.0,
-            max_depenetration_velocity=1.0,
+            max_depenetration_velocity=100.0,
+            enable_gyroscopic_forces=True,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+            enabled_self_collisions=False,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
         ),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
